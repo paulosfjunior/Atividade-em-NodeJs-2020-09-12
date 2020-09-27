4d57054e3463b00c0e04c58b6f537c3cc15fdd66
Paulo Freitas <paulosfjunior@gmail.com>
2020-09-26 23:54:29 -0300

    adicionado gerarHash e validarSenha ao model customer

--- src/app/models/customer.js
+++ src/app/models/customer.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -6,5 +7,13 @@ const customerSchema = new Schema({
   email: {type: String, required: true},
   senha: {type: String, required: true}
 });
+
+customerSchema.methods.gerarHash = function(senha){
+  return bcrypt.hashSync (senha, bcrypt.genSaltSync(8, null));
+}
+
+customerSchema.methods.validarSenha = function(senha){
+  return bcrypt.compareSync(senha, this.senha);
+}
 
 module.exports = mongoose.model('Usuario', customerSchema);
